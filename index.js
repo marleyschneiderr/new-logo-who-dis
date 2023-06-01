5c817d5168d4d9549b171fe3a2570b614bb16e6f
marleyschneiderr <marley33@uw.edu>
2023-05-31 23:35:14 -0700

    Adding validators in index.js to ensure correct answer is inputed when prompted

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // requiring inquirer for node package management
 const inquirer = require("inquirer");
-//
 const fs = require("fs");
+const { Triangle, Circle, Square } = require("./lib/shapes");
 
 inquirer
   .prompt([
@@ -9,28 +9,44 @@ inquirer
       type: "input",
       name: "symbols",
       message: "Please enter up to three characters/symbols:",
+      // validate used to ensure the user provides a valid input for the symbols/wordColor/and colorShape inputs
+      // if invalid input is received, an error message is displayed, then the user is prompted again until they put in a correct one
+      validate: (input) => {
+        if (input.length > 3) {
+          return "Please enter up to three characters/symbols.";
+        }
+        return true;
+      },
     },
-
     {
       type: "input",
       name: "wordColor",
       message: "Input the color you would prefer your character/symbols to be:",
+      validate: (input) => {
+        if (!input) {
+          return "Please enter a color:";
+        }
+        return true;
+      },
     },
-
     {
       type: "list",
       name: "shape",
-      message: "Select a specific shape for you logo:",
+      message: "Select a specific shape for your logo:",
       choices: ["triangle", "circle", "square"],
     },
-
     {
       type: "input",
       name: "colorShape",
       message: "Input the color you would prefer your shape to be:",
+      validate: (input) => {
+        if (!input) {
+          return "Please enter a color:";
+        }
+        return true;
+      },
     },
   ])
-
   .then((info) => {
     const { symbols, wordColor, shape, colorShape } = info;
     var svgInstance;
@@ -43,7 +59,12 @@ inquirer
         break;
       case "square":
         svgInstance = new Square(colorShape, wordColor, symbols);
+        break;
     }
 
     fs.writeFileSync("./examples/logo.svg", svgInstance.render());
+    console.log("Logo generated successfully!!");
+  })
+  .catch((error) => {
+    console.error("An error occurred while creating the logo:", error);
   });
